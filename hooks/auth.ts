1ea067c0d03bf95e2d1bf38a7951c16a107a911d
mehulrana017 <mehulrana159@gmail.com>
2025-11-25 18:59:54 +0530

    basic ui

--- hooks/auth.ts
+++ hooks/auth.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable no-console */
 import { useMutation } from "@tanstack/react-query";
 import { apiClient } from "@/lib/api/client";
