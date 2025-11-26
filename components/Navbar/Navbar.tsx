3f5f2a6c2244e42a7eadf3f1692c9895af049d22
mehulrana017 <mehulrana159@gmail.com>
2025-11-27 01:30:19 +0530

    fixes

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -26,18 +26,15 @@ export function Navbar() {
   }
 
   const isAuthenticated = !!state.currentUser;
-  const cartItemCount = state.cart?.items.reduce((sum, item) => sum + item.quantity, 0) || 0;
+  const cartItemCount =
+    state.cart?.items.reduce((sum, item) => sum + item.quantity, 0) || 0;
 
   return (
     <>
       <CheckoutSlider ref={checkoutSliderRef} />
 
       <nav className="relative flex items-center justify-between p-4 lg:px-6">
-        <div className="block flex-none md:hidden">
-          {/* <Suspense fallback={null}>
-          <MobileMenu menu={menu} />
-        </Suspense> */}
-        </div>
+        <div className="block flex-none md:hidden"></div>
         <div className="flex w-full items-center">
           <div className="flex w-full md:w-1/3">
             <Link
@@ -50,21 +47,6 @@ export function Navbar() {
                 {SITE_NAME}
               </div>
             </Link>
-            {/* {menu.length ? (
-            <ul className="hidden gap-6 text-sm md:flex md:items-center">
-              {menu.map((item: Menu) => (
-                <li key={item.title}>
-                  <Link
-                    href={item.path}
-                    prefetch={true}
-                    className="text-neutral-500 underline-offset-4 hover:text-black hover:underline dark:text-neutral-400 dark:hover:text-neutral-300"
-                  >
-                    {item.title}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          ) : null} */}
           </div>
           <div className="hidden justify-center md:flex md:w-1/3">
             <Suspense fallback={<SearchSkeleton />}>
